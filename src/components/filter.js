b44d11f8614d8fce5461d58469629a5a58a78a99
akhil <akhbharti@gmail.com>
2020-08-05 05:09:48 +0530

    filter option and styling added

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,7 +1,29 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {styles} from './styles/filterStyles'
-const years = [
-  "2006",
+import {Button, withStyles} from '@material-ui/core'
+import { green } from "@material-ui/core/colors";
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(green[500]),
+    backgroundColor: green[500],
+    borderRadius: 5,
+    width: 75,
+    margin: "10px auto",
+    "&:hover": {
+      backgroundColor: green[700],
+    },
+    "&.active": {
+      backgroundColor: theme.palette.primary.main,
+      color: `${theme.palette.secondary.contrastText}!important`,
+      transition: "border-radius .15s cubic-bezier(0.4,0.0,0.2,1)",
+    },
+  },
+}))(Button);
+const filterOptions= {
+  boolOptions:[
+"true","false"
+  ],
+  years:["2006",
   "2007",
   "2008",
   "2009",
@@ -15,30 +37,81 @@ const years = [
   "2017",
   "2018",
   "2019",
-  "2020",
-];
+  "2020",]
+}
+
 function Filter(props){
   // const [year, setYear] = useState("");
   // const [isLaunchSuccess, setIsLaunchSuccess] = useState(true);
   // const [isLandingSuccess, setIsLandingSuccess] = useState(true);
   const classes = styles(props)
-
+  const {years, boolOptions} = filterOptions
+  const isActive=key=>{
+    return "active"
+  }
    return (
-    <div className={classes.filterList}>
-      Filters
-      <div className={classes.filterListWrapper}>
-        <div className={classes.filterListHeader}>Launch Year</div>
-        <div className={classes.filterListContent}></div>
-      </div>
-      <div className={classes.filterListWrapper}>
-        <div className={classes.filterListHeader}>Successful Launch</div>
-        <div className={classes.filterListContent}></div>
-      </div>
-      <div className={classes.filterListWrapper}>
-        <div className={classes.filterListHeader}>Successful Landing</div>
-        <div className={classes.filterListContent}></div>
-      </div>
-    </div>
-  );
+     <div className={classes.filterList}>
+       Filters
+       <div className={classes.filterListWrapper}>
+         <div className={classes.filterListHeader}>Launch Year</div>
+         <div className={classes.filterListContent}>
+           {years &&
+             years.map((year) => {
+               return (
+                 <div className="classes.yearButton">
+                   <ColorButton
+                     variant="contained"
+                     color="primary"
+                     key={year}
+                     className={isActive(year)}
+                     // onClick={() => showYear(data, setYear)}
+                   >
+                     {year}
+                   </ColorButton>
+                 </div>
+               );
+             })}
+         </div>
+       </div>
+       <div className={classes.filterListWrapper}>
+         <div className={classes.filterListHeader}>Successful Launch</div>
+         <div className={classes.filterListContent}>
+           {boolOptions &&
+             boolOptions.map((option) => (
+               <div className="classes.yearButton">
+                 <ColorButton
+                   variant="contained"
+                   color="primary"
+                   key={option}
+                   className={isActive(option)}
+                   // onClick={() => showYear(data, setYear)}
+                 >
+                   {option}
+                 </ColorButton>
+               </div>
+             ))}
+         </div>
+       </div>
+       <div className={classes.filterListWrapper}>
+         <div className={classes.filterListHeader}>Successful Landing</div>
+         <div className={classes.filterListContent}>
+           {boolOptions &&
+             boolOptions.map((option) => (
+               <div className="classes.yearButton">
+                 <ColorButton
+                   variant="contained"
+                   color="primary"
+                   key={option}
+                   className={isActive(option)}
+                   // onClick={() => showYear(data, setYear)}
+                 >
+                   {option}
+                 </ColorButton>
+               </div>
+             ))}
+         </div>
+       </div>
+     </div>
+   );
 }export default Filter
 
--- src/components/styles/filterStyles.js
+++ src/components/styles/filterStyles.js
@@ -1,29 +1,33 @@
 import { makeStyles } from "@material-ui/styles";
 
 export const styles = makeStyles({
-  filterList:{
-    backgroundColor: "rgb(253, 249, 244)",
-    border: "1px solid #c2c3c4",
-    borderRadius: "2px",
-    width: "80%",
-    padding: "30px",
-    margin: "10px",
-  },
-  filterListWrapper:{
-    display: "flex",
-    justifyContent: "space-around",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  filterListContent:{
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    width: "200px",
-    justifyContent: "space-between",
-  },
-  filterListHeader:{
-    fontSize: "20px",
-    backgroundColor: "#cad0d6",
-  }
-})
+         filterList: {
+           backgroundColor: "rgb(253, 249, 244)",
+           border: "1px solid #c2c3c4",
+           borderRadius: "2px",
+           width: "80%",
+           padding: "30px",
+           margin: "10px",
+         },
+         filterListWrapper: {
+           display: "flex",
+           justifyContent: "space-around",
+           flexDirection: "column",
+           alignItems: "center",
+         },
+         filterListContent: {
+           display: "flex",
+           flexDirection: "row",
+           flexWrap: "wrap",
+           width: "200px",
+           justifyContent: "space-between",
+         },
+         filterListHeader: {
+           fontSize: "20px",
+           backgroundColor: "#cad0d6",
+         },
+         yearButton:{
+           display:"flex",
+           width:100
+         },
+       });
