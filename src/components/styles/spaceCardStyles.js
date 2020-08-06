fbf7a5c4b4880129f801d41ef7eead783404f481
akhil <akhbharti@gmail.com>
2020-08-07 00:06:35 +0530

    card box shadow added on hover

--- src/components/styles/spaceCardStyles.js
+++ src/components/styles/spaceCardStyles.js
@@ -1,9 +1,9 @@
 import { makeStyles } from "@material-ui/core";
 export const useStyles = makeStyles((theme) => ({
   root: {
-    height:500,
+    height: 500,
     maxWidth: 345,
-    cursor:"default",
+    cursor: "default",
     // height:"100%",
     margin: "10px",
     borderRadius: 5,
@@ -11,14 +11,17 @@ export const useStyles = makeStyles((theme) => ({
       width: "45%",
     },
     [theme.breakpoints.up("lg")]: { width: "22%" },
-    ""
+    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+    transition: "all 0.3s cubic-bezier(.25,.8,.25,1)",
+    "&:hover": {
+      boxShadow: " 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+    },
   },
   media: {
     height: 200,
     objectFit: "scale-down",
     textAlign: "center",
     backgroundColor: "#afa6a61f",
-
   },
   section: {
     display: "flex",
